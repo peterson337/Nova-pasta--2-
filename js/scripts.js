d4711e6f97542c49f74dbb472a9c5a5e76aa5ce8
Pterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2022-12-29 15:31:46 -0200

    testando

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+ // Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#rdit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+ // Funções
+
+ // Eventos
